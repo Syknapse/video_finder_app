00476192e004f198cd09819c2a3cb3ff8bb9a71c
Syknapse <29199184+Syknapse@users.noreply.github.com>
2021-11-20 15:56:58 +0100

    Create API directory and method to get Pexels vids

---
+++ src/api/media.js
@@ -0,0 +1,6 @@
+import { createClient } from 'pexels'
+
+const { REACT_APP_PEXELS_KEY } = process.env
+const client = createClient(REACT_APP_PEXELS_KEY)
+
+export const getVideos = ({ query, perPage }) => client.videos.search({ query, per_page: perPage }).then(data => data)
