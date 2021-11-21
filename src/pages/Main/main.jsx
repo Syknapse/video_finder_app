7409937b1233c411b637846f61cf02cf0c23e4c2
Syknapse <29199184+Syknapse@users.noreply.github.com>
2021-11-21 18:39:33 +0100

    Add logic to play all videos sequentially

--- src/pages/Main/main.jsx
+++ src/pages/Main/main.jsx
@@ -5,14 +5,23 @@ import { Player } from '@components'
 import './main.css'
 
 const Main = () => {
+  const vidEl = useRef(null)
   const [displayVideos, setDisplayVideos] = useState([])
   const [currentVid, setCurrentVid] = useState(null)
+  const [vidIndex, setVidIndex] = useState(0)
   const [isLoading, setIsLoading] = useState(false)
   const [values, setValues] = useState({
     query: '',
     numberOfVideos: 10,
   })
-  const vidEl = useRef(null)
+
+  useEffect(() => {
+    setCurrentVid(displayVideos[vidIndex])
+  }, [vidIndex, displayVideos])
+
+  useEffect(() => {
+    if (currentVid) vidEl.current.load()
+  }, [currentVid])
 
   const getData = async ({ query, perPage }) => {
     setIsLoading(true)
@@ -27,23 +36,13 @@ const Main = () => {
     }
   }
 
-  useEffect(() => {
-    setCurrentVid(displayVideos[0])
-  }, [displayVideos])
-
-  useEffect(() => {
-    if (currentVid) vidEl.current.load()
-  }, [currentVid])
+  const getNextVideo = () => (vidIndex === displayVideos.length - 1 ? setVidIndex(0) : setVidIndex(prev => ++prev))
 
   const handleSubmit = e => {
     e.preventDefault()
     const { query, numberOfVideos } = values
     if (!query) return
     getData({ query, perPage: numberOfVideos })
-  }
-
-  const handleEnded = () => {
-    setCurrentVid(displayVideos[6])
   }
 
   return (
@@ -55,7 +54,7 @@ const Main = () => {
         handleSearchChange={e => setValues({ ...values, query: e.target.value })}
         handleNumChange={e => setValues({ ...values, numberOfVideos: e.target.value })}
       />
-      <Player video={currentVid} onEnded={() => handleEnded()} vidEl={vidEl} isLoading={isLoading} />
+      <Player video={currentVid} onEnded={() => getNextVideo()} vidEl={vidEl} isLoading={isLoading} />
     </main>
   )
 }
